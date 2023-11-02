51d3a6abe6d18646648ff29b5af77f40cb224463
LN97 <34336338+LN97@users.noreply.github.com>
2023-11-02 10:22:27 +0000

    creation of element2

---
+++ babylonProj2/element2/src/createStartScene.ts
@@ -0,0 +1,116 @@
+// importing BABYLON-------------------------
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import {
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    Vector4,
+    HemisphericLight,
+    MeshBuilder,
+    Mesh,
+    Light,
+    Camera,
+    Engine,
+    StandardMaterial,
+    Texture,
+  } from "@babylonjs/core";
+  //-------------------------------------------
+  //-------------------------------------------
+
+
+  //Middle of code-----------------------------
+  
+  function createBox(scene: Scene, px: number, py: number, pz: number, sx: number, sy: number, sz: number) {
+    let box = MeshBuilder.CreateBox("box",{size: 1}, scene);
+    box.position = new Vector3(px, py, pz);
+    box.scaling = new Vector3(sx, sy, sz);
+
+    return box;
+  }
+
+  // faced box function 
+  function createFacedBox(scene: Scene, px: number, py: number, pz: number) {
+    const mat = new StandardMaterial("Mat");
+    const texture = new Texture("https://assets.babylonjs.com/environment/numbers.jpg")
+    mat.diffuseTexture = texture;
+
+    const faceUV = new Array(6);
+
+ // for
+    for (var i = 0; i < 6; i++) {
+      faceUV[i] = new Vector4((i+1));
+    }
+
+
+  }
+
+  
+  function createLight(scene: Scene) {
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+    return light;
+  }
+  
+  function createSphere(scene: Scene) {
+    let sphere = MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2, segments: 32 },
+      scene,
+    );
+    sphere.position.y = 1;
+    return sphere;
+  }
+  
+  function createGround(scene: Scene) {
+    let ground = MeshBuilder.CreateGround(
+      "ground",
+      { width: 6, height: 6 },
+      scene,
+    );
+    return ground;
+    
+  }
+  //--------------------------------------------------
+  //--------------------------------------------------
+  //Bottom of Code
+  
+  function createArcRotateCamera(scene: Scene) {
+    let camAlpha = -Math.PI / 2,
+      camBeta = Math.PI / 2.5,
+      camDist = 10,
+      camTarget = new Vector3(0, 0, 0);
+    let camera = new ArcRotateCamera(
+      "camera1",
+      camAlpha,
+      camBeta,
+      camDist,
+      camTarget,
+      scene,
+    );
+    camera.attachControl(true);
+    return camera;
+  }
+  
+  export default function createStartScene(engine: Engine) {
+    interface SceneData {
+      scene: Scene;
+      box?: Mesh;
+      facebox?:Mesh;
+      light?: Light;
+      sphere?: Mesh;
+      ground?: Mesh;
+      camera?: Camera;
+    }
+  
+    let that: SceneData = { scene: new Scene(engine) };
+    that.scene.debugLayer.show();
+  
+    that.box = createBox(that.scene, 2, 5, 3, 3, 2, 1);
+    //that.facebox = createFacedBox(that.scene, 2, 5, 3,);
+    that.light = createLight(that.scene);
+    that.sphere = createSphere(that.scene);
+    that.ground = createGround(that.scene);
+    that.camera = createArcRotateCamera(that.scene);
+    return that;
+  }
