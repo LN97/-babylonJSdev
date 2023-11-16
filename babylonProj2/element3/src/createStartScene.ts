4b28bdf09d94d26f665f294f71d3e27b9960cda4
LN97 <34336338+LN97@users.noreply.github.com>
2023-11-16 11:15:44 +0000

    creating element 3

--- babylonProj2/element3/src/createStartScene.ts
+++ babylonProj2/element3/src/createStartScene.ts
@@ -2,6 +2,8 @@
 //TOP OF CODE - IMPORTING BABYLONJS
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
+
+
 import {
     Scene,
     ArcRotateCamera,
@@ -27,75 +29,152 @@ import {
     SceneLoader,
     ActionManager,
     ExecuteCodeAction,
-
+    AnimationPropertiesOverride,
   } from "@babylonjs/core";
+  import HavokPhysics from "@babylonjs/havok";
+  import { HavokPlugin, PhysicsAggregate, PhysicsShapeType } from "@babylonjs/core";
+
   //----------------------------------------------------
   
   //----------------------------------------------------
-  //MIDDLE OF CODE - FUNCTIONS
-  function importPlayerMesh(scene, x: number, y: number) {
-    let item = SceneLoader.ImportMesh("", "./models/", "dummy3.babylon", scene,
-   function(newMeshes) {
-    let mesh = newMeshes[0];
 
-    scene.onBeforeRenderObservable.add(()=> {
-      if (keyDownMap["w"] || keyDownMap["ArrowUp"]) {
-      mesh.position.z += 0.1;
-      mesh.rotation.y = 0;
-      }
-      if (keyDownMap["a"] || keyDownMap["ArrowLeft"]) {
-      mesh.position.x -= 0.1;
-      mesh.rotation.y = 3 * Math.PI / 2;
-      }
-      if (keyDownMap["s"] || keyDownMap["ArrowDown"]) {
-      mesh.position.z -= 0.1;
-      mesh.rotation.y = 2 * Math.PI / 2;
-      }
-      if (keyDownMap["d"] || keyDownMap["ArrowRight"]) {
-      mesh.position.x += 0.1;
-      mesh.rotation.y = Math.PI / 2;
-      }
+  // initialisation of phsyics
+  let initializedHavok;
+
+HavokPhysics().then((havok) => {
+ initializedHavok = havok;
+});
+
+const havokInstance = await HavokPhysics();
+const havokPlugin = new HavokPlugin(true, havokInstance);
+
+globalThis.HK = await HavokPhysics();
+
+
+  //MIDDLE OF CODE - FUNCTIONS
+  let keyDownMap: any[] = [];
+  let currentSpeed: number = 0.1;
+  let walkingSpeed: number = 0.1;
+  let runningSpeed: number = 0.4;
+
+  function importPlayerMesh(scene: Scene, collider: Mesh , x: number, y: number) {
+    let tempItem = { flag: false } 
+    let item: any = SceneLoader.ImportMesh("", "./models/", "dummy3.babylon", scene, function(newMeshes, particleSystems, skeletons) {
+      let mesh = newMeshes[0];
+      let skeleton = skeletons[0];
+      skeleton.animationPropertiesOverride = new AnimationPropertiesOverride();
+      skeleton.animationPropertiesOverride.enableBlending = true;
+      skeleton.animationPropertiesOverride.blendingSpeed = 0.05;
+      skeleton.animationPropertiesOverride.loopMode = 1; 
+
+      let walkRange: any = skeleton.getAnimationRange("YBot_Walk");
+      // let runRange: any = skeleton.getAnimationRange("YBot_Run");
+      // let leftRange: any = skeleton.getAnimationRange("YBot_LeftStrafeWalk");
+      // let rightRange: any = skeleton.getAnimationRange("YBot_RightStrafeWalk");
+      // let idleRange: any = skeleton.getAnimationRange("YBot_Idle");
+
+      let animating: boolean = false;
+
+      scene.onBeforeRenderObservable.add(()=> {
+        let keydown: boolean = false;
+        let shiftdown: boolean = false;
+        if (keyDownMap["w"] || keyDownMap["ArrowUp"]) {
+          mesh.position.z += 0.1;
+          mesh.rotation.y = 0;
+          keydown = true;
+        }
+        if (keyDownMap["a"] || keyDownMap["ArrowLeft"]) {
+          mesh.position.x -= 0.1;
+          mesh.rotation.y = 3 * Math.PI / 2;
+          keydown = true;
+        }
+        if (keyDownMap["s"] || keyDownMap["ArrowDown"]) {
+          mesh.position.z -= 0.1;
+          mesh.rotation.y = 2 * Math.PI / 2;
+          keydown = true;
+        }
+        if (keyDownMap["d"] || keyDownMap["ArrowRight"]) {
+          mesh.position.x += 0.1;
+          mesh.rotation.y = Math.PI / 2;
+          keydown = true;
+        }
+        if (keyDownMap["Shift"] || keyDownMap["LeftShift"]) {
+          currentSpeed = runningSpeed;
+          shiftdown = true;
+        } else {
+          currentSpeed = walkingSpeed;
+          shiftdown = false;
+        }
+
+        if (keydown) {
+          if (!animating) {
+            animating = true;
+            scene.beginAnimation(skeleton, walkRange.from, walkRange.to, true);
+          }
+        } else {
+          animating = false;
+          scene.stopAnimation(skeleton);
+        }
+
+        //collision 
+        if (mesh.intersectsMesh(collider)) {
+
+          console.log("COLLIDED");
+       
+       }
+       
+
       });
 
-
+      //Physics collison
+      item = mesh;
+      let playerAggregate = new PhysicsAggregate(item, PhysicsShapeType.CAPSULE, { mass: 0 }, scene);
+      playerAggregate.body.disablePreStep = false;
 
     });
-
-    
     return item;
-    } 
-
-    function actionManager(scene: Scene){
-      scene.actionManager = new ActionManager(scene);
-      scene.actionManager.registerAction(
-      new ExecuteCodeAction(
-      {
-      trigger: ActionManager.OnKeyDownTrigger,
-      //parameters: 'w'
-      },
-      function(evt) {keyDownMap[evt.sourceEvent.key] = true; }
-      )
-      );
-      scene.actionManager.registerAction(
-      new ExecuteCodeAction(
-      {
-      trigger: ActionManager.OnKeyUpTrigger
-     
-      },
-      function(evt) {keyDownMap[evt.sourceEvent.key] = false; }
-      )
-      );
-      return scene.actionManager;
-     } 
-
-  //Create more detailed ground
-  function createGround(scene: Scene) {
-    const ground = MeshBuilder.CreateGround("ground", {height: 10, width: 10, subdivisions: 4});
-
-
-    return ground;
   }
 
+  function actionManager(scene: Scene){
+    scene.actionManager = new ActionManager(scene);
+
+    scene.actionManager.registerAction(
+      new ExecuteCodeAction(
+        {
+          trigger: ActionManager.OnKeyDownTrigger,
+          //parameters: 'w'
+        },
+        function(evt) {keyDownMap[evt.sourceEvent.key] = true; }
+      )
+    );
+    scene.actionManager.registerAction(
+      new ExecuteCodeAction(
+        {
+          trigger: ActionManager.OnKeyUpTrigger
+        },
+        function(evt) {keyDownMap[evt.sourceEvent.key] = false; }
+      )
+    );
+    return scene.actionManager;
+  } 
+
+  function createBox(scene: Scene, x: number, y: number, z: number){
+    let box: Mesh = MeshBuilder.CreateBox("box");
+    box.position.x = x;
+    box.position.y = y;
+    box.position.z = z;
+    const boxAggregate = new PhysicsAggregate(box, PhysicsShapeType.BOX, { mass: 1 }, scene);
+    return box;
+   }
+   
+   function createGround(scene: Scene) {
+    const ground: Mesh = MeshBuilder.CreateGround("ground", {height: 10, width: 10,
+   subdivisions: 4});
+    const groundAggregate = new PhysicsAggregate(ground, PhysicsShapeType.BOX, { mass: 0 },
+   scene);
+    return ground;
+   } 
+  //----------------------------------------------------------------------------------------------
   //Create Skybox
   function createSkybox(scene: Scene) {
     //Skybox
@@ -111,7 +190,6 @@ import {
   }
 
 
-  //----------------------------------------------------------------------------------------------
 
   function createAnyLight(scene: Scene, index: number, px: number, py: number, pz: number, colX: number, colY: number, colZ: number, mesh: Mesh) {
     // only spotlight, point and directional can cast shadows in BabylonJS
@@ -177,34 +255,39 @@ import {
   export default function createStartScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
-      ground?: Mesh; 
+      box?: Mesh;
+      ground?: Mesh;
+      importMesh?: any;
+      actionManager?: any;
       skybox?: Mesh;
       light?: Light;
       hemisphericLight?: HemisphericLight;
       camera?: Camera;
-      importMesh?: any;
-      actionManager?: any; 
-
     }
   
     let that: SceneData = { scene: new Scene(engine) };
     that.scene.debugLayer.show();
+    that.scene.enablePhysics(new Vector3(0, -9.8, 0), havokPlugin);
+
+
+    //any further code goes here-----------
 
 
 
-
-    //any further code goes here
-    //-------------------------------------------------------
+ 
+    //-------------------------------------
+    that.box = createBox(that.scene, that.box, 2, 2, 2);
     that.ground = createGround(that.scene);
-    that.skybox = createSkybox(that.scene);
+
     that.importMesh = importPlayerMesh(that.scene, 0, 0);
     that.actionManager = actionManager(that.scene);
 
-
-
+    // that.ground = createGround(that.scene);
+    that.skybox = createSkybox(that.scene);
     //Scene Lighting & Camera
     that.hemisphericLight = createHemiLight(that.scene);
     that.camera = createArcRotateCamera(that.scene);
+    
     return that;
   }
   //----------------------------------------------------
