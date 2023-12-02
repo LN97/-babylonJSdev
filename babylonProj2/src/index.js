beda8da877d97509d974d5f5f5fcebb616c91f39
LN97 <rolisnini@gmail.com>
2023-12-02 16:30:04 +0000

    update

--- babylonProj2/src/index.js
+++ babylonProj2/src/index.js
@@ -6,11 +6,8 @@ let canvas = document.createElement("canvas");
 canvas.id = CanvasName;
 canvas.classList.add("background-canvas");
 document.body.appendChild(canvas);
-
 let eng = new Engine(canvas, true, {}, true);
-
 let startScene = createStartScene(eng);
-
 eng.runRenderLoop(() => {
     startScene.scene.render();
 });
